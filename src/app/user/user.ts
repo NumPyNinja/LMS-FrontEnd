7359a700d2584b1f2cb86711113f15716be7cc62
Riya Pawar <numpyninja01@gmail.com>
2022-04-20 12:23:48 -0400

    removed user

---
+++ src/app/user/user.ts
@@ -0,0 +1,19 @@
+export interface User {
+    emailAddress: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: number;
+    location: string;
+    timezone: string;
+    linkedUrl: string;
+    ugProgram: string;
+    skill: string;
+    experience: number;
+    comments: string;
+    fileType: string;
+    userRole: string;
+    batch: string;
+    program: string;
+    userName: string;
+    password: string;
+}
