21f13a9a8b0725d30d758cf93a2cbf475281d0e9
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 16:24:54 -0400

    troubeshooting

--- src/app/program/program.service.ts
+++ src/app/program/program.service.ts
@@ -15,7 +15,7 @@ export class ProgramService {
   getPrograms(): Observable<Program[]> {
     // return this.httpClient.get<Program[]>('assets/Programs.json')
 
-    return this.httpClient.get<Program[]>( this.url + "/allPrograms");;
+    return this.httpClient.get<Program[]>( "https://lms-phase2.herokuapp.com/lms/allPrograms");
   }
 
   addProgram(program: Program): Observable<Program> {
