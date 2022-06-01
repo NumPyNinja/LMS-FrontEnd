a52e61847361d313df20fc0e75e0c3fda3e94c87
neeshunarang <101758557+neeshunarang@users.noreply.github.com>
2022-06-01 11:08:56 -0700

    added AssignmentList

--- src/app/assignment/assignment.ts
+++ src/app/assignment/assignment.ts
@@ -1,2 +1,16 @@
-export class Assignment {
+export interface Assignment {
+    
+    assignmentId?: number,
+    assignmentName?: string,
+    assignmentDescription?: string,
+    assignmentComments?: string,
+    assignmentDueDate?: Date,
+    assignmentPathAttach1?: string,
+    assignmentPathAttach2?: string,
+    assignmentPathAttach3?: string,
+    assignmentPathAttach4?: string,
+    assignmentPathAttach5?: string,
+    assignmentCreatedBy?: string,
+    assignmentBatchId?: number,
+    assignmentGraderId?: string
 }
