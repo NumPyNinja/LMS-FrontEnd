d28c0188146bba77b1944246006ac2f4c016767d
Priya <priya@Priyas-MacBook-Pro.local>
2022-06-01 20:09:59 -0400

    heroku

--- sever.js
+++ sever.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/LMS-FrontEnd'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/lms-frontend-phase2'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/LMS-FrontEnd'}),
+    res.sendFile('index.html', {root: 'dist/lms-frontend-phase2/'}),
 );
 
-app.listen(process.env.PORT || 8080)
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
